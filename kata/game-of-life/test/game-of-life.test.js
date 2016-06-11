b47337f369758525df9675f99acd622c30aeca8c
Greg <gregmnagy@gmail.com>
2016-06-11 16:54:53 -0700

    Created the Matrix, destroyed the width

--- kata/game-of-life/test/game-of-life.test.js
+++ kata/game-of-life/test/game-of-life.test.js
@@ -1,9 +1,16 @@
 var assert = require('chai').assert; 
+var Matrix = require('../matrix.js');
 
 describe('The Game of Life', function () {
     it('should have a matrix', function () {
         var matrix = new Matrix(); 
-
+    
         assert.notEqual(matrix, undefined);
+    });
+    it('should have a width', function () {
+        var matrix = new Matrix();
+        matrix.width = 100;
+
+        assert.equal(matrix.getWidth(), 100);
     });
 });
