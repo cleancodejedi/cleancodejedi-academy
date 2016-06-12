4a6123bf07ba6b94738446b8b8c6876b17009100
Ray Joseph de Castro <zengineer@me.com>
2016-06-11 17:04:30 -0700

    feature. implemented width, and added test to validate height.

--- kata/game-of-life/matrix.js
+++ kata/game-of-life/matrix.js
@@ -1,3 +1,9 @@
- function Matrix (){}
+function Matrix () { 
+    this.width = 0; 
+}
+
+Matrix.prototype.getWidth = function () {
+    return this.width; 
+};
 
 module.exports = Matrix;
--- kata/game-of-life/test/game-of-life.test.js
+++ kata/game-of-life/test/game-of-life.test.js
@@ -1,16 +1,26 @@
-var assert = require('chai').assert; 
+var assert = require('chai').assert;
 var Matrix = require('../matrix.js');
 
-describe('The Game of Life', function () {
-    it('should have a matrix', function () {
-        var matrix = new Matrix(); 
-    
-        assert.notEqual(matrix, undefined);
-    });
-    it('should have a width', function () {
-        var matrix = new Matrix();
-        matrix.width = 100;
+describe('In the Game of Life,', function () {
+    describe('The matrix', function () {
+        it('should exist', function () {
+            var matrix = new Matrix();
 
-        assert.equal(matrix.getWidth(), 100);
+            assert.notEqual(matrix, undefined);
+        });
+
+        it('should have a width', function () {
+            var matrix = new Matrix();
+            matrix.width = 100;
+
+            assert.equal(matrix.getWidth(), 100);
+        });
+
+        it('should have a height', function () {
+            var matrix = new Matrix();
+            matrix.height = 108;
+
+            assert.equal(matrix.getHeight(), 108);
+        });
     });
 });
