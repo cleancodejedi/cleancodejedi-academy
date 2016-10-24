8182fe78ad003240d0d767522be675e0b330e0c4
raydecastro <captray108@gmail.com>
2016-10-23 19:25:40 -0700

    refactor. changed property name.

--- kata/game-of-life/no-primitives/test/numeric.test.js
+++ kata/game-of-life/no-primitives/test/numeric.test.js
@@ -4,10 +4,10 @@ import {expect} from "chai";
 import Numeric from "../src/numeric.js";
 
 describe("Numeric", () => {
-    it("shall have an n property", () => {
+    it("shall have an value property", () => {
         let numeric = new Numeric();
 
-        numeric.n = 100;
-        expect(numeric.n).to.equal(100);
+        numeric.value = 100;
+        expect(numeric.value).to.equal(100);
     });
 });
